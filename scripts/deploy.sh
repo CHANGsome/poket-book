b737eb74b39810276024779900fec2d7cca99efa
zhouhua.tang <zhouhua.tang@shopee.com>
2021-03-09 15:38:06 +0800

    修改标题

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+yarn build &&
+cd build &&
+git init &&
+git add. &&
+git commit -m "deploy" &&
+git branch build &&
+git checkout build &&
+git remote add origin git@github.com:CHANGsome/poket-book.git &&
+git push -u origin build -f &&
+cd -
